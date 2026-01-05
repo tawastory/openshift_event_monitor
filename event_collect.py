0c6479ba0e540945155db3a7844bcdc6644fa270
Your Name <you@example.com>
2026-01-06 01:07:13 +0900

    Update Openshift v4 API

--- event_collect.py
+++ event_collect.py
@@ -1,29 +1,17 @@
-from kubernetes import client, config
-from openshift.dynamic import DynamicClient
-import kubernetes.config
+from kubernetes import config, dynamic
+from kubernetes.client import api_client
+
 import traceback
 import sys
 import datetime
 
-def get_client(**kwargs):
-    try:
-        k8s_client = config.new_client_from_config()
-    except config.config_exception.ConfigException:
-        traceback.print_exc()
-        sys.exit(1)
-
-    return DynamicClient(k8s_client)
-
-token_auth = dict(
-    api_key={'authorization': 'Bearer {}'.format('<api-token>')},
-    host='<host>',
-    verify_ssl=False
-)
-
 def main():
     try:
-        client = get_client(**token_auth)
-        v1_events = client.resources.get(api_version='events.k8s.io/v1beta1', kind='Event')
+        client = dynamic.DynamicClient(
+            api_client.ApiClient(configuration=config.load_kube_config())
+        )
+
+        v1_events = client.resources.get(api_version='events.k8s.io/v1', kind='Event')
 
         event_list = v1_events.get()
 
